14b96126bbb5090fe1cc27158726f0ec20e25de2
mazixiao <2649198873@qq.com>
2022-06-25 20:24:20 +0800

    docs(git): y

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'element-plus/dist/index.css';
+import './styles/index.scss';
 
 createApp(App).use(store).use(router).mount('#app');
---
+++ src/views/login/loginIndex.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    login
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+<style lang="scss">
+
+</style>
