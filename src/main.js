a3ccdb61819380794aec7917da8ce85fa5feea7a
mazixiao <2649198873@qq.com>
2022-06-25 18:56:23 +0800

    feat(git): add element-plus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+/* jshint esversion: 6 */
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'element-plus/dist/index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-button type="primary">按钮</el-button>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ElButton } from 'element-plus'
+
+export default defineComponent({
+  components: { ElButton }
+})
+</script>
+<style lang="scss">
+
+</style>
